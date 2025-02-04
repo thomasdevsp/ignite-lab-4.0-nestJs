e84df6796ac298a3575e7f11f64f75eb1e7b78c3
Thomasdevsp <thomasdevsp@gmail.com>
2025-02-04 17:56:34 -0300

    feat: Adding one test and changing a tests sintaxe

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,15 +1,19 @@
-import { Content } from "./content"
+import { Content } from "./content";
 
-test('it should be abe to create a notification content', () => {
-  const content = new Content('Você recebeu uma solicitação de amizade')
+describe('Notifications Content', () => {
+  it('should be abe to create a notification content', () => {
+    const content = new Content('Você recebeu uma solicitação de amizade')
 
-  expect(content).toBeTruthy()
+    expect(content).toBeTruthy()
+  });
+
+  it('should not be abe to create a notification content with less than 5 characters', () => {
+    expect(() => new Content('Voc')).toThrow()
+  });
+
+  it('should not be abe to create a notification content with more than 240 characters', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow()
+  });
 })
 
-test('it should not be abe to create a notification content with less than 5 characters', () => {
-  expect(() => new Content('Voc')).toThrow()
-})
 
-test('it should not be abe to create a notification content with more than 240 characters', () => {
-  expect(() => new Content('a'.repeat(241))).toThrow()
-})
